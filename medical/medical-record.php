7c1ff144b5a5a6d3ec9ae4bf9fee01837556c9c9
petrk94 <petrk94@users.noreply.github.com>
2018-10-10 18:18:03 +0200

    cleaned comments from code to reduce load time

--- medical/medical-record.php
+++ medical/medical-record.php
@@ -61,10 +61,6 @@ sec_session_start();
 <div class="wrapper">
 	<div class="sidebar" data-background-color="white" data-active-color="danger">
 
-    <!--
-		Tip 1: you can change the color of the sidebar's background using: data-background-color="white | black"
-		Tip 2: you can change the color of the active button using the data-active-color="primary | info | success | warning | danger"
-	-->
 
     	<div class="sidebar-wrapper">
             <div class="logo">
@@ -216,37 +212,6 @@ sec_session_start();
 		
 		
 		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
      <!--   <div class="content"> -->
             <div class="container-fluid">
                 <div class="row">
@@ -297,36 +262,6 @@ mysqli_close ($connection); //Make sure to close out the database connection
                 </div>
             </div>
         </div>
-
-        <footer class="footer">
-		<!--
-            <div class="container-fluid">
-                <nav class="pull-left">
-                    <ul>
-
-                        <li>
-                            <a href="http://www.creative-tim.com">
-                                Creative Tim
-                            </a>
-                        </li>
-                        <li>
-                            <a href="http://blog.creative-tim.com">
-                               Blog
-                            </a>
-                        </li>
-                        <li>
-                            <a href="http://www.creative-tim.com/license">
-                                Licenses
-                            </a>
-                        </li>
-                    </ul>
-                </nav>
-				<div class="copyright pull-right">
-                    &copy; <script>document.write(new Date().getFullYear())</script>, made with <i class="fa fa-heart heart"></i> by <a href="http://www.creative-tim.com">Creative Tim</a>
-                </div>
-            </div>
-			-->
-        </footer>
 
 
     </div>
