a8638608135148ec12223688845381e4c3ed6896
petrk94 <petrk94@users.noreply.github.com>
2018-10-10 15:08:58 +0200

    updated db_connect.php

--- includes/db_connect.php
+++ includes/db_connect.php
@@ -19,7 +19,9 @@
 
 include_once 'psl-config.php';   // Needed because functions.php is not included
 
-$mysqli = new mysqli(HOST, USER, PASSWORD, DATABASE);
+$connection = mysqli_connect(HOST, USER, PASSWORD);
+mysqli_select_db($connection, DATABASE);
+
 if ($mysqli->connect_error) {
     header("Location: ../error.php?err=Unable to connect to MySQL");
     exit();
