f6547afb4cf009c48c7f29ad8e92b054e099bef9
petrk94 <petrk94@users.noreply.github.com>
2018-10-10 15:01:07 +0200

    fixed link

--- register.php
+++ register.php
@@ -15,8 +15,8 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-include_once '../includes/register.inc.php';
-include_once '../includes/functions.php';
+include_once 'includes/register.inc.php';
+include_once 'includes/functions.php';
 ?>
 <!DOCTYPE html>
 <html>
