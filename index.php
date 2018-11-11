15b260c37156df6d61bc25ef38bc6bede53cab35
petrk94 <petrk94@users.noreply.github.com>
2018-11-11 01:01:19 +0100

    updated the beautiful login.php and renamed to index.php

--- index.php
+++ index.php
@@ -27,30 +27,46 @@ if (login_check($mysqli) == true) {
 }
 ?>
 <!DOCTYPE html>
+
 <html>
     <head>
         <title>Secure Login: Log In</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
         <link rel="stylesheet" href="assets/styles/main.css" />
         <script type="text/JavaScript" src="assets/js/sha512.js"></script> 
         <script type="text/JavaScript" src="assets/js/forms.js"></script> 
     </head>
-    <body>
-        <?php
-        if (isset($_GET['error'])) {
-            echo '<p class="error">Error Logging In!</p>';
-        }
-        ?> 
-        <form action="includes/process_login.php" method="post" name="login_form"> 			
-            Email: <input type="text" name="email" />
-            Password: <input type="password" 
-                             name="password" 
-                             id="password"/>
-            <input type="button" 
-                   value="Login" 
-                   onclick="formhash(this.form, this.form.password);" /> 
-        </form>
-        <p>If you don't have a login, please <a href="register.php">register</a></p>
-        <p>If you are done, please <a href="includes/logout.php">log out</a>.</p>
-        <p>You are currently logged <?php echo $logged ?>.</p>
-    </body>
+<link href="assets/css/login.css" rel="stylesheet" />
+<div class="wrapper fadeInDown">
+  <div id="formContent">
+    <!-- Tabs Titles -->
+    <h2 class="active"> Sign In </h2>
+
+    <!-- Icon -->
+    <div class="fadeIn first">
+      <img src="assets/ownhealthrecord_logo.png" id="icon" alt="User Icon" />
+    </div>
+
+    <!-- Login Form -->
+    <form action="includes/process_login.php" method="post" name="login_form">
+      <input type="text" id="login" class="fadeIn second" name="email" placeholder="login">
+      <input type="password" id="password" class="fadeIn third" name="password" placeholder="password">
+      <input type="submit" class="fadeIn fourth" value="Login" onclick="formhash(this.form, this.form.password);">
+    </form>
+	
+	
+      
+	  
+	  
+	  
+    <!-- Remind Passowrd -->
+    <div id="formFooter">
+	<p>If you don't have a login, please <a href="signup.php">Create Account</a></p>
+	<p>You are currently logged <?php echo $logged ?>.</p>
+	<p>If you are done, please <a href="includes/logout.php">log out</a>.</p>
+    </div>
+
+  </div>
+</div>
 </html>
