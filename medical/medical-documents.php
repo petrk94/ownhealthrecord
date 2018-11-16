5be1b88c06f0fe584a0de449c14663b348adb6b1
petrk94 <petrk94@users.noreply.github.com>
2018-11-16 23:17:39 +0100

    added php listing function from third party developer

--- medical/medical-documents.php
+++ medical/medical-documents.php
@@ -62,14 +62,15 @@ sec_session_start();
     <link href="../assets/css/themify-icons.css" rel="stylesheet">
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.0/css/all.css">
 
-
+   <link rel="stylesheet" href="./.style.css">
+   <script src="./.sorttable.js"></script>
 	
 </head>
 <body>
 <?php if (login_check($mysqli) == true) : ?>
 
 <div class="wrapper">
-	<div class="sidebar" data-background-color="white" data-active-color="danger">
+	<div class="sidebar" data-background-color="white" data-active-color="danger" style="width: 280px;">
 
 
     	<div class="sidebar-wrapper">
@@ -218,19 +219,174 @@ sec_session_start();
             </div>		
 		
 		
+<div class="container-fluid">
+	<h2>Testing Version - not final</h2>
+
+	<table class="sortable">
+	    <thead>
+		<tr>
+			<th>Filename</th>
+			<th>Type</th>
+			<th>Size</th>
+			<th>Date Modified</th>
+		</tr>
+	    </thead>
+	    <tbody><?php
+
+	// Adds pretty filesizes
+	function pretty_filesize($file) {
+		$size=filesize($file);
+		if($size<1024){$size=$size." Bytes";}
+		elseif(($size<1048576)&&($size>1023)){$size=round($size/1024, 1)." KB";}
+		elseif(($size<1073741824)&&($size>1048575)){$size=round($size/1048576, 1)." MB";}
+		else{$size=round($size/1073741824, 1)." GB";}
+		return $size;
+	}
+
+ 	// Checks to see if veiwing hidden files is enabled
+	if($_SERVER['QUERY_STRING']=="hidden")
+	{$hide="";
+	 $ahref="./";
+	 $atext="Hide";}
+	else
+	{$hide=".";
+	 $ahref="./?hidden";
+	 $atext="Show";}
+
+	 // Opens directory
+	 $myDirectory=opendir("./uploads/");
+
+	// Gets each entry
+	while($entryName=readdir($myDirectory)) {
+	   $dirArray[]=$entryName;
+	}
+
+	// Closes directory
+	closedir($myDirectory);
+
+	// Counts elements in array
+	$indexCount=count($dirArray);
+
+	// Sorts files
+	sort($dirArray);
+
+	// Loops through the array of files
+	for($index=0; $index < $indexCount; $index++) {
+
+	// Decides if hidden files should be displayed, based on query above.
+	    if(substr("$dirArray[$index]", 0, 1)!=$hide) {
+
+	// Resets Variables
+		$favicon="";
+		$class="file";
+
+	// Gets File Names
+		$name=$dirArray[$index];
+		$namehref=$dirArray[$index];
+
+	// Gets Date Modified
+		$modtime=date("M j Y g:i A", filemtime($dirArray[$index]));
+		$timekey=date("YmdHis", filemtime($dirArray[$index]));
+
+
+	// Separates directories, and performs operations on those directories
+		if(is_dir($dirArray[$index]))
+		{
+				$extn="&lt;Directory&gt;";
+				$size="&lt;Directory&gt;";
+				$sizekey="0";
+				$class="dir";
+
+			// Gets favicon.ico, and displays it, only if it exists.
+				if(file_exists("$namehref/favicon.ico"))
+					{
+						$favicon=" style='background-image:url($namehref/favicon.ico);'";
+						$extn="&lt;Website&gt;";
+					}
+
+			// Cleans up . and .. directories
+				if($name=="."){$name=". (Current Directory)"; $extn="&lt;System Dir&gt;"; $favicon=" style='background-image:url($namehref/.favicon.ico);'";}
+				if($name==".."){$name=".. (Parent Directory)"; $extn="&lt;System Dir&gt;";}
+		}
+
+	// File-only operations
+		else{
+			// Gets file extension
+			$extn=pathinfo($dirArray[$index], PATHINFO_EXTENSION);
+
+			// Prettifies file type
+			switch ($extn){
+				case "png": $extn="PNG Image"; break;
+				case "jpg": $extn="JPEG Image"; break;
+				case "jpeg": $extn="JPEG Image"; break;
+				case "svg": $extn="SVG Image"; break;
+				case "gif": $extn="GIF Image"; break;
+				case "ico": $extn="Windows Icon"; break;
+
+				case "txt": $extn="Text File"; break;
+				case "log": $extn="Log File"; break;
+				case "htm": $extn="HTML File"; break;
+				case "html": $extn="HTML File"; break;
+				case "xhtml": $extn="HTML File"; break;
+				case "shtml": $extn="HTML File"; break;
+				case "php": $extn="PHP Script"; break;
+				case "js": $extn="Javascript File"; break;
+				case "css": $extn="Stylesheet"; break;
+
+				case "pdf": $extn="PDF Document"; break;
+				case "xls": $extn="Spreadsheet"; break;
+				case "xlsx": $extn="Spreadsheet"; break;
+				case "doc": $extn="Microsoft Word Document"; break;
+				case "docx": $extn="Microsoft Word Document"; break;
+
+				case "zip": $extn="ZIP Archive"; break;
+				case "htaccess": $extn="Apache Config File"; break;
+				case "exe": $extn="Windows Executable"; break;
+
+				default: if($extn!=""){$extn=strtoupper($extn)." File";} else{$extn="Unknown";} break;
+			}
+
+			// Gets and cleans up file size
+				$size=pretty_filesize($dirArray[$index]);
+				$sizekey=filesize($dirArray[$index]);
+		}
+
+	// Output
+	 echo("
+		<tr class='$class'>
+			<td><a href='./uploads/$namehref'$favicon class='name'>$name</a></td>
+			<td><a href='./uploads/$namehref'>$extn</a></td>
+			<td sorttable_customkey='$sizekey'><a href='./uploads/$namehref'>$size</a></td>
+			<td sorttable_customkey='$timekey'><a href='./uploads/$namehref'>$modtime</a></td>
+		</tr>");
+	   }
+	}
+	?>
+
+	    </tbody>
+	</table>
+
+</div>
+		
+		
+		
+		
+		
+
 		
 		
 		
 		
 		
 		
-            <div class="container-fluid">
-				<div style="text-align: center;padding-top: 200px;"class="row">
-				  <div class="col-sm-4"></div>
-				  <div class="col-sm-4"><h3>This page is still in development</h3></div>
-				  <div class="col-sm-4"></div>
-				</div>
-            </div>
+		
+		
+		
+		
+		
+		
+		
+		
         </div>
 
         <footer class="footer">
