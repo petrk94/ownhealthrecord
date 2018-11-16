d5813ae58f9e60a6c6032547ad08cf38bbd807a0
petrk94 <petrk94@users.noreply.github.com>
2018-11-16 17:30:44 +0100

    added upload.php script for uploading files, first test

---
+++ medical/upload/upload.php
@@ -0,0 +1,43 @@
+<?php
+$upload_folder = '/'; //Das Upload-Verzeichnis
+$filename = pathinfo($_FILES['datei']['name'], PATHINFO_FILENAME);
+$extension = strtolower(pathinfo($_FILES['datei']['name'], PATHINFO_EXTENSION));
+ 
+ 
+//Überprüfung der Dateiendung
+$allowed_extensions = array('png', 'jpg', 'jpeg', 'gif');
+if(!in_array($extension, $allowed_extensions)) {
+ die("Ungültige Dateiendung. Nur png, jpg, jpeg und gif-Dateien sind erlaubt");
+}
+ 
+//Überprüfung der Dateigröße
+$max_size = 500*1024; //500 KB
+if($_FILES['datei']['size'] > $max_size) {
+ die("Bitte keine Dateien größer 500kb hochladen");
+}
+ 
+//Überprüfung dass das Bild keine Fehler enthält
+if(function_exists('exif_imagetype')) { //exif_imagetype erfordert die exif-Erweiterung
+ $allowed_types = array(IMAGETYPE_PNG, IMAGETYPE_JPEG, IMAGETYPE_GIF);
+ $detected_type = exif_imagetype($_FILES['datei']['tmp_name']);
+ if(!in_array($detected_type, $allowed_types)) {
+ die("Nur der Upload von Bilddateien ist gestattet");
+ }
+}
+ 
+//Pfad zum Upload
+$new_path = $upload_folder.$filename.'.'.$extension;
+ 
+//Neuer Dateiname falls die Datei bereits existiert
+if(file_exists($new_path)) { //Falls Datei existiert, hänge eine Zahl an den Dateinamen
+ $id = 1;
+ do {
+ $new_path = $upload_folder.$filename.'_'.$id.'.'.$extension;
+ $id++;
+ } while(file_exists($new_path));
+}
+ 
+//Alles okay, verschiebe Datei an neuen Pfad
+move_uploaded_file($_FILES['datei']['tmp_name'], $new_path);
+echo 'Bild erfolgreich hochgeladen: <a href="'.$new_path.'">'.$new_path.'</a>';
+?>
