b9a90559bbff658789efa9adfcd8a82b5aa73975
petrk94 <petrk94@users.noreply.github.com>
2019-04-18 15:39:27 +0200

    fixed upload issues and accessing it

--- medical/medical-documents.php
+++ medical/medical-documents.php
@@ -38,7 +38,7 @@ sec_session_start();
 	<link rel="icon" type="image/png" sizes="96x96" href="assets/img/favicon.png">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
 
-	<title>Paper Dashboard by Creative Tim</title>
+	<title>OwnHealthRecord</title>
 
 	<meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
     <meta name="viewport" content="width=device-width" />
@@ -206,9 +206,11 @@ sec_session_start();
 								<div class="content">
 								
 									<form id="upload" method="post" action="../upload.php" enctype="multipart/form-data" style="display: flex;">
-										Select image to upload:
+										<p>Select image to upload:<br>
 										<input type="file" name="datei" id="datei">
-										<input type="submit" value="Upload Image" name="submit">
+										<br>
+										<input type="submit" value="Upload Image" name="submit" style="width: 100%;">
+										</p>
 									</form>
 								</div>
 							
@@ -220,7 +222,6 @@ sec_session_start();
 		
 		
 <div class="container-fluid">
-	<h2>Testing Version - not final</h2>
 
 	<table class="sortable">
 	    <thead>
@@ -231,140 +232,120 @@ sec_session_start();
 			<th>Date Modified</th>
 		</tr>
 	    </thead>
-	    <tbody><?php
+	    <tbody>
 
-	// Adds pretty filesizes
-	function pretty_filesize($file) {
-		$size=filesize($file);
-		if($size<1024){$size=$size." Bytes";}
-		elseif(($size<1048576)&&($size>1023)){$size=round($size/1024, 1)." KB";}
-		elseif(($size<1073741824)&&($size>1048575)){$size=round($size/1048576, 1)." MB";}
-		else{$size=round($size/1073741824, 1)." GB";}
-		return $size;
-	}
+		<?php
 
- 	// Checks to see if veiwing hidden files is enabled
-	if($_SERVER['QUERY_STRING']=="hidden")
-	{$hide="";
-	 $ahref="./";
-	 $atext="Hide";}
-	else
-	{$hide=".";
-	 $ahref="./?hidden";
-	 $atext="Show";}
-
-	 // Opens directory
-	 $myDirectory=opendir("../uploads");
-
-	// Gets each entry
-	while($entryName=readdir($myDirectory)) {
-	   $dirArray[]=$entryName;
-	}
-
-	// Closes directory
-	closedir($myDirectory);
-
-	// Counts elements in array
-	$indexCount=count($dirArray);
-
-	// Sorts files
-	sort($dirArray);
-
-	// Loops through the array of files
-	for($index=0; $index < $indexCount; $index++) {
-
-	// Decides if hidden files should be displayed, based on query above.
-	    if(substr("$dirArray[$index]", 0, 1)!=$hide) {
-
-	// Resets Variables
-		$favicon="";
-		$class="file";
-
-	// Gets File Names
-		$name=$dirArray[$index];
-		$namehref=$dirArray[$index];
-
-	// Gets Date Modified
-		$modtime=date("M j Y g:i A", filemtime($dirArray[$index]));
-		$timekey=date("YmdHis", filemtime($dirArray[$index]));
-
-
-	// Separates directories, and performs operations on those directories
-		if(is_dir($dirArray[$index]))
-		{
-				$extn="&lt;Directory&gt;";
-				$size="&lt;Directory&gt;";
-				$sizekey="0";
-				$class="dir";
-
-			// Gets favicon.ico, and displays it, only if it exists.
-				if(file_exists("$namehref/favicon.ico"))
-					{
-						$favicon=" style='background-image:url($namehref/favicon.ico);'";
-						$extn="&lt;Website&gt;";
-					}
-
-			// Cleans up . and .. directories
-				if($name=="."){$name=". (Current Directory)"; $extn="&lt;System Dir&gt;"; $favicon=" style='background-image:url($namehref/.favicon.ico);'";}
-				if($name==".."){$name=".. (Parent Directory)"; $extn="&lt;System Dir&gt;";}
-		}
-
-	// File-only operations
-		else{
-			// Gets file extension
-			$extn=pathinfo($dirArray[$index], PATHINFO_EXTENSION);
-
-			// Prettifies file type
-			switch ($extn){
-				case "png": $extn="PNG Image"; break;
-				case "jpg": $extn="JPEG Image"; break;
-				case "jpeg": $extn="JPEG Image"; break;
-				case "svg": $extn="SVG Image"; break;
-				case "gif": $extn="GIF Image"; break;
-				case "ico": $extn="Windows Icon"; break;
-
-				case "txt": $extn="Text File"; break;
-				case "log": $extn="Log File"; break;
-				case "htm": $extn="HTML File"; break;
-				case "html": $extn="HTML File"; break;
-				case "xhtml": $extn="HTML File"; break;
-				case "shtml": $extn="HTML File"; break;
-				case "php": $extn="PHP Script"; break;
-				case "js": $extn="Javascript File"; break;
-				case "css": $extn="Stylesheet"; break;
-
-				case "pdf": $extn="PDF Document"; break;
-				case "xls": $extn="Spreadsheet"; break;
-				case "xlsx": $extn="Spreadsheet"; break;
-				case "doc": $extn="Microsoft Word Document"; break;
-				case "docx": $extn="Microsoft Word Document"; break;
-
-				case "zip": $extn="ZIP Archive"; break;
-				case "htaccess": $extn="Apache Config File"; break;
-				case "exe": $extn="Windows Executable"; break;
-
-				default: if($extn!=""){$extn=strtoupper($extn)." File";} else{$extn="Unknown";} break;
+			// Adds pretty filesizes
+			function pretty_filesize($file) {
+				$size=filesize($file);
+				if($size<1024){$size=$size." Bytes";}
+				elseif(($size<1048576)&&($size>1023)){$size=round($size/1024, 1)." KB";}
+				elseif(($size<1073741824)&&($size>1048575)){$size=round($size/1048576, 1)." MB";}
+				else{$size=round($size/1073741824, 1)." GB";}
+				return $size;
 			}
 
-			// Gets and cleans up file size
-				$size=pretty_filesize($dirArray[$index]);
-				$sizekey=filesize($dirArray[$index]);
+        // Opens directory
+        $myDirectory=opendir("../uploads/");
+        
+        // Gets each entry
+        while($entryName=readdir($myDirectory)) {
+          $dirArray[]=$entryName;
+        }
+
+        
+        // Closes directory
+        closedir($myDirectory);
+        
+        // Counts elements in array
+        $indexCount=count($dirArray);
+        
+        // Sorts files
+        sort($dirArray);
+        
+        // Loops through the array of files
+        for($index=0; $index < $indexCount; $index++) {
+        
+          if($_SERVER['QUERY_STRING']=="hidden")
+          {$hide="";
+          $ahref="./";
+          $atext="Hide";}
+          else
+          {$hide=".";
+          $ahref="./?hidden";
+          $atext="Show";}
+          if(substr("$dirArray[$index]", 0, 1) != $hide) {
+			  
+          // Gets File Names
+          $name=$dirArray[$index];
+          $namehref=$dirArray[$index];
+          
+          // Gets Extensions 
+          // old/outdated   $extn=findexts($dirArray[$index]); 
+		  $extn=pathinfo($dirArray[$index]);
+          
+          // Gets file size 
+          $size=number_format(filesize("../uploads/" . $dirArray[$index]));
+          
+          // Gets Date Modified Data
+          $modtime=date("M j Y g:i A", filemtime("../uploads/" . $dirArray[$index]));
+          $timekey=date("YmdHis", filemtime("../uploads/" . $dirArray[$index]));
+          
+          // Prettifies File Types, add more to suit your needs.
+          switch ($extn['extension']){
+            case "png": $extn="PNG Image"; break;
+            case "jpg": $extn="JPEG Image"; break;
+            case "svg": $extn="SVG Image"; break;
+            case "gif": $extn="GIF Image"; break;
+            case "ico": $extn="Windows Icon"; break;
+            
+            case "txt": $extn="Text File"; break;
+            case "log": $extn="Log File"; break;
+            case "htm": $extn="HTML File"; break;
+            case "php": $extn="PHP Script"; break;
+            case "js": $extn="Javascript"; break;
+            case "css": $extn="Stylesheet"; break;
+            case "pdf": $extn="PDF Document"; break;
+            
+            case "zip": $extn="ZIP Archive"; break;
+            case "bak": $extn="Backup File"; break;
+            
+            default: $extn=strtoupper($extn)." File"; break;
+          }
+
+		// Gets and cleans up file size
+			$size=pretty_filesize("../uploads/" . $dirArray[$index]);
+			$sizekey=filesize("../uploads/" . $dirArray[$index]);
+			
+          // Separates directories
+          if(is_dir($dirArray[$index])) {
+            $extn="&lt;Directory&gt;"; 
+            $size="&lt;Directory&gt;"; 
+            $class="dir";
+          } else {
+            $class="file";
+          }
+          
+          // Cleans up . and .. directories 
+          if($name=="."){$name=". (Current Directory)"; $extn="&lt;System Dir&gt;";}
+          if($name==".."){$name=".. (Parent Directory)"; $extn="&lt;System Dir&gt;";}
+          
+          // Print 'em
+          print("
+          <tr class='$class'>
+            <td><a href='../uploads/$namehref'>$name</a></td>
+            <td><a href='../uploads/$namehref'>$extn</a></td>
+            <td><a href='../uploads/$namehref'>$size</a></td>
+            <td sorttable_customkey='$timekey'><a href='../uploads/$namehref'>$modtime</a></td>
+          </tr>");
+          }
 		}
-
-	// Output
-	 echo("
-		<tr class='$class'>
-			<td><a href='./uploads/$namehref'$favicon class='name'>$name</a></td>
-			<td><a href='./uploads/$namehref'>$extn</a></td>
-			<td sorttable_customkey='$sizekey'><a href='./uploads/$namehref'>$size</a></td>
-			<td sorttable_customkey='$timekey'><a href='./uploads/$namehref'>$modtime</a></td>
-		</tr>");
-	   }
-	}
-	?>
-
-	    </tbody>
-	</table>
+      ?>
+      </tbody>
+    </table>
+    
+  </div>		
 
 </div>
 		
