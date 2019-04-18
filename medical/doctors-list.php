fae9ef206e3702ce3b1c0bc29f05c4892b48646f
petrk94 <petrk94@users.noreply.github.com>
2019-04-18 15:39:07 +0200

    updated Application title from few pages

--- medical/doctors-list.php
+++ medical/doctors-list.php
@@ -38,7 +38,7 @@ sec_session_start();
 	<link rel="icon" type="image/png" sizes="96x96" href="assets/img/favicon.png">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
 
-	<title>Paper Dashboard by Creative Tim</title>
+	<title>OwnHealthRecord</title>
 
 	<meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
     <meta name="viewport" content="width=device-width" />
