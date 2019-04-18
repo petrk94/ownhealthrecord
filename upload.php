733b6b167259a6f111152371d718f041fd30dd6c
petrk94 <petrk94@users.noreply.github.com>
2019-04-18 15:46:30 +0200

    translated comments from german to english

--- upload.php
+++ upload.php
@@ -1,43 +1,43 @@
 <?php
-$upload_folder = 'uploads/'; //Das Upload-Verzeichnis
+$upload_folder = 'uploads/'; //The Upload directory
 $filename = pathinfo($_FILES['datei']['name'], PATHINFO_FILENAME);
 $extension = strtolower(pathinfo($_FILES['datei']['name'], PATHINFO_EXTENSION));
  
  
-//Überprüfung der Dateiendung
+//Check file extension
 $allowed_extensions = array('png', 'jpg', 'jpeg', 'gif');
 if(!in_array($extension, $allowed_extensions)) {
- die("Ungültige Dateiendung. Nur png, jpg, jpeg und gif-Dateien sind erlaubt");
+ die("not allowed file extension. Only PNG, JPG, JPEG and GIF are allowed");
 }
  
-//Überprüfung der Dateigröße
+//Check file size
 $max_size = 500*1024; //500 KB
 if($_FILES['datei']['size'] > $max_size) {
- die("Bitte keine Dateien größer 500kb hochladen");
+ die("Please dont upload files larger than 500kb");
 }
  
-//Überprüfung dass das Bild keine Fehler enthält
-if(function_exists('exif_imagetype')) { //exif_imagetype erfordert die exif-Erweiterung
+//Check whether the image contains no issues
+if(function_exists('exif_imagetype')) { //exif_imagetype need exif-extension
  $allowed_types = array(IMAGETYPE_PNG, IMAGETYPE_JPEG, IMAGETYPE_GIF);
  $detected_type = exif_imagetype($_FILES['datei']['tmp_name']);
  if(!in_array($detected_type, $allowed_types)) {
- die("Nur der Upload von Bilddateien ist gestattet");
+ die("Only upload of image files is allowed");
  }
 }
  
-//Pfad zum Upload
+//Path to upload location
 $new_path = $upload_folder.$filename.'.'.$extension;
  
-//Neuer Dateiname falls die Datei bereits existiert
-if(file_exists($new_path)) { //Falls Datei existiert, hänge eine Zahl an den Dateinamen
+//New file name, if file already exist
  $id = 1;
+if(file_exists($new_path)) { //If file exists, add number to the file name
  do {
  $new_path = $upload_folder.$filename.'_'.$id.'.'.$extension;
  $id++;
  } while(file_exists($new_path));
 }
  
-//Alles okay, verschiebe Datei an neuen Pfad
+//If everything fine, move file to new location
 move_uploaded_file($_FILES['datei']['tmp_name'], $new_path);
-echo 'Bild erfolgreich hochgeladen: <a href="'.$new_path.'">'.$new_path.'</a>';
+echo 'Image uploaded successfully to: <a href="'.$new_path.'">'.$new_path.'</a>';
 ?>
